bb3c3212ae8747cef5e8bc0989faacb14036b93b
chenkeyan <wx_5c7ac24408f6477d922043368e60e4c3@git.code.tencent.com>
2019-01-16 14:14:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* CSS3属性-webkit-font-smoothing字体抗锯齿渲染 使用后字体看起来会更清晰 */
+  -webkit-font-smoothing: antialiased; 
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
